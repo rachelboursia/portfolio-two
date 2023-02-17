6ec8b2d03d7f6efe96c56ee47e1894cda88c2c7c
rachelboursia <rachel.l.boursia@gmail.com>
2023-02-17 10:27:20 -0800

    removes logo

--- form-handler.php
+++ form-handler.php
@@ -4,5 +4,20 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
+$email_form = 'info@rachelboursia.github.io/portfolio-two/.com';
+$email_subject = 'New Form Submission';
+$email_body = "User Name: $name.\n".
+                "User Email $vistor_email.\n".
+                "Subject: $subject.\n".
+                "User Message: $message .\n";
 
+$to = 'rachel.l.boursia@gmail.com';
+
+$headers = 'From: $email_form \r\n';
+
+$headers .= 'Reply-To: $visitor_email \r\n';
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
 ?>
